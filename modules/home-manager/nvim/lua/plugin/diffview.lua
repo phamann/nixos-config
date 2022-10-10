6a5d61a18b748029a33d6e91f2f6c203860edd9c
Patrick Hamann <patrickhamann@gmail.com>
2022-10-10 10:07:05 +0100

    Fix diffview leader mapping

--- modules/home-manager/nvim/lua/plugin/diffview.lua
+++ modules/home-manager/nvim/lua/plugin/diffview.lua
@@ -1,5 +1,5 @@
 require("diffview").setup()
 
-vim.keymap.set("n", "<leader><leader>dh", "<Cmd>DiffviewFileHistory<CR>", { desc = "diff history" })
-vim.keymap.set("n", "<leader><leader>do", "<Cmd>DiffviewOpen<CR>", { desc = "diff open" })
-vim.keymap.set("n", "<leader><leader>dc", "<Cmd>DiffviewClose<CR>", { desc = "diff close" })
+vim.keymap.set("n", "<leader>dh", "<Cmd>DiffviewFileHistory<CR>", { desc = "diff history" })
+vim.keymap.set("n", "<leader>do", "<Cmd>DiffviewOpen<CR>", { desc = "diff open" })
+vim.keymap.set("n", "<leader>dc", "<Cmd>DiffviewClose<CR>", { desc = "diff close" })
